8a5a1db23fcf30a502c6c2f9534f32f7cf9b74b3
JasnoorMaan <jasnoorm0001@gmail.com>
2023-08-23 01:11:11 +0530

    start

---
+++ src/components/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Home.css';
+
+const Home: React.FC = () => {
+  return (
+    <div className="glassmorphic-container">
+      <div className="glassmorphic-content">
+        {/* Your content goes here */}
+        <h1>Welcome to Glassmorphic Home</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
